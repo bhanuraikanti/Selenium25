6d1ac48716af8d30a32fb769888134f94569225a
Bhanuprasad <bhanuprasadraikanti@gmail.com>
2025-04-15 16:08:01 +0530

    Update Edge.java

--- src/basicpractice/Edge.java
+++ src/basicpractice/Edge.java
@@ -8,9 +8,9 @@ public class Edge {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-      WebDriver driver = new EdgeDriver();
-  	  driver.manage().window().maximize();
-      driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
+     //  WebDriver driver = new EdgeDriver();
+  	  // driver.manage().window().maximize();
+     //  driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
 
 		//single window
 		String parentWindowHandle = driver.getWindowHandle();
