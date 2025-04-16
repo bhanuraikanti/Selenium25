9edd430305467652081d5604d08956010397fa0c
bhanu prasad <bhanu prasad@LAPTOP-F7OO9IRC>
2025-04-16 11:22:35 +0530

    sixth commit

--- src/basicpractice/Edge.java
+++ src/basicpractice/Edge.java
@@ -7,19 +7,19 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class Edge {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-      WebDriver driver = new EdgeDriver();
- 	   driver.manage().window().maximize();
-      driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
-
-		//single window
-	
-		 String parentWindowHandle = driver.getWindowHandle();
-		 driver.findElement(By.cssSelector("button[onclick=\"newBrowserWindow()\"]")).click();
-
-	// BHANU PRASAD 
-          
-  
+//		// TODO Auto-generated method stub
+//      WebDriver driver = new EdgeDriver();
+// 	   driver.manage().window().maximize();
+//      driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
+//
+//		//single window
+//	
+//		 String parentWindowHandle = driver.getWindowHandle();
+//		 driver.findElement(By.cssSelector("button[onclick=\"newBrowserWindow()\"]")).click();
+//
+//	// BHANU PRASAD 
+//          
+//  
 	}
 
 }
