9b9eb240fbcbe256fe5d2f20995a9e48f1ce4189
Bhanuprasad <bhanuprasadraikanti@gmail.com>
2025-04-16 11:23:41 +0530

    Update Edge.java

--- src/basicpractice/Edge.java
+++ src/basicpractice/Edge.java
@@ -8,18 +8,18 @@ public class Edge {
 
 	public static void main(String[] args) {
 //		// TODO Auto-generated method stub
-//      WebDriver driver = new EdgeDriver();
-// 	   driver.manage().window().maximize();
-//      driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
-//
-//		//single window
-//	
-//		 String parentWindowHandle = driver.getWindowHandle();
-//		 driver.findElement(By.cssSelector("button[onclick=\"newBrowserWindow()\"]")).click();
-//
-//	// BHANU PRASAD 
-//          
-//  
+     WebDriver driver = new EdgeDriver();
+	   driver.manage().window().maximize();
+     driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
+
+		//single window
+	
+		 String parentWindowHandle = driver.getWindowHandle();
+		 driver.findElement(By.cssSelector("button[onclick=\"newBrowserWindow()\"]")).click();
+
+	// BHANU PRASAD 
+         
+ 
 	}
 
 }
